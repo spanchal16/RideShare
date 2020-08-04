88601b99f7de6f4b536f848e94c8aea8ca91d873
Breej Vania <breejvania@gmail.com>
2020-08-04 19:26:51 -0300

    build updated

---
+++ build/precache-manifest.e65502d71fdb1a14feefe9c767107a40.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eece6357ff50a7cbe3150a83a8120775",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eab094d41a645be0d7f1",
+    "url": "/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "e05218d8d1d2454380e8",
+    "url": "/static/css/main.5587ea45.chunk.css"
+  },
+  {
+    "revision": "eab094d41a645be0d7f1",
+    "url": "/static/js/2.19cc9c6a.chunk.js"
+  },
+  {
+    "revision": "92e43ec1ae976ba4d89e6168d2e02348",
+    "url": "/static/js/2.19cc9c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e05218d8d1d2454380e8",
+    "url": "/static/js/main.2409383d.chunk.js"
+  },
+  {
+    "revision": "fb17d285815b51d42c37",
+    "url": "/static/js/runtime-main.8fd84bac.js"
+  },
+  {
+    "revision": "465faff2d70a72c2363cee2388910829",
+    "url": "/static/media/A2FooterFb.465faff2.png"
+  },
+  {
+    "revision": "7742981d5b7a464d4200a26eb5a6b512",
+    "url": "/static/media/A2FooterInsta.7742981d.png"
+  },
+  {
+    "revision": "0452ac38302c16481666342d3d532c96",
+    "url": "/static/media/A2FooterTwitter.0452ac38.png"
+  },
+  {
+    "revision": "90e9df32d7902b97e7d208aaf4530656",
+    "url": "/static/media/background4.90e9df32.jpg"
+  },
+  {
+    "revision": "3cf1399e5ae07cbb5f7d1091c220edc1",
+    "url": "/static/media/carpool.3cf1399e.png"
+  },
+  {
+    "revision": "b64405516244ceb363b369a9bff4ca6e",
+    "url": "/static/media/logo.b6440551.png"
+  },
+  {
+    "revision": "5d18dc271bceac0b99c2d7497c28efd3",
+    "url": "/static/media/rideshare.5d18dc27.png"
+  }
+]);
