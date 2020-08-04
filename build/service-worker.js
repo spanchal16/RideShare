66c7df9098e5d80cec5cc97409b54ab8b9b830dd
JigarMakwana <31387807+JigarMakwana@users.noreply.github.com>
2020-08-04 02:05:29 -0300

    changed default home

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cd5ef09061ea253af0a9f0ed25255c3.js"
+  "/precache-manifest.7b3bf104cdb2312dc61293236bad417f.js"
 );
 
 self.addEventListener('message', (event) => {
