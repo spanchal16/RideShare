778772795f816e5d02c93217b182f23910eb4e03
Sagar Moghe <sg286595@dal.ca>
2020-08-04 18:55:14 -0300

    Bump button fix

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d41274d7632f05023769f58e82db60d.js"
+  "/precache-manifest.0ba2978221046c98e29f0fa0f29bd521.js"
 );
 
 self.addEventListener('message', (event) => {
